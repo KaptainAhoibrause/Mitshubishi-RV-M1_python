4d78c4da947639729b5d37f3b56461a64a7c3d86
KaptainAhoibrause <132769719+KaptainAhoibrause@users.noreply.github.com>
2024-06-21 20:09:44 +0200

    added first class + functions

---
+++ src/main.py
@@ -0,0 +1,27 @@
+class Movemaster:
+    def nesting(self):
+        print("NST")
+
+    def orgigin(self):
+        print("OG")
+
+    def write_hand(self):
+        print("WH")
+        print("<answerY")
+
+    def write_mem(self):
+        print("DR")
+        print("<answer>")
+
+    def set_speed(self):
+        print("SP <0-9> <H or L>")  # H is faster brake, accelerate than L
+
+
+robot = Movemaster()
+
+if __name__ == "__main__":
+    while 1337:
+        try:
+            exec(input(">>>"))
+        except NameError:
+            print("Wrong name")
