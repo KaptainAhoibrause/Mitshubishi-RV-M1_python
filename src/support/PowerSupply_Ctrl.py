c2df4417e85f62911092a08c6357cee9624068ec
Mika-Rsbg <ro-mika@web.de>
2024-11-03 16:46:33 +0100

    added timeout

--- src/support/PowerSupply_Ctrl.py
+++ src/support/PowerSupply_Ctrl.py
@@ -4,9 +4,18 @@ HOST = "192.168.100.10"  # The server's hostname or IP address
 PORT = 5025  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while 42:
-        usrin = input(">>>") + "\n"
-        s.sendall(usrin.encode('ascii'))
-        data = s.recv(1024)
-        print("Geräteantwort:", data.decode('ascii'))
+    s.settimeout(5)  # Set a timeout of 5 seconds
+    try:
+        s.connect((HOST, PORT))
+        while True:
+            usrin = input(">>>") + "\n"
+            s.sendall(usrin.encode('ascii'))
+            try:
+                data = s.recv(1024)
+                print("Geräteantwort:", data.decode('ascii'))
+            except socket.timeout:
+                print("Timeout: Keine Antwort vom Gerät.")
+    except socket.timeout:
+        print("Timeout: Verbindung zum Server konnte nicht hergestellt werden.")
+    except Exception as e:
+        print("Ein Fehler ist aufgetreten:", e)
