319c87c8f62ec636ba4061f4810702aa9b097e89
KaptainAhoibrause <132769719+KaptainAhoibrause@users.noreply.github.com>
2024-11-11 10:50:44 +0100

    added toggle switches

--- src/support/PowerSupply_Ctrl/GUI.py
+++ src/support/PowerSupply_Ctrl/GUI.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout
+# from PyQt6.QtCore import Qt
 
 
 class MainWindow(QWidget):
@@ -7,7 +8,50 @@ class MainWindow(QWidget):
         super().__init__(*args, **kwargs)
 
         # set the window title
-        self.setWindowTitle('Hello World')
+        self.setWindowTitle('PowerSupplyUI')
+
+        # channel label
+        chnl1Label = QLabel()
+        chnl1Label.setText("CH 1")
+
+        chnl2Label = QLabel()
+        chnl2Label.setText("CH 2")
+
+        chnl3Label = QLabel()
+        chnl3Label.setText("CH 3")
+
+        chnl4Label = QLabel()
+        chnl4Label.setText("CH 4")
+
+        # channel buttons
+        chnl1Button = QPushButton()
+        chnl1Button.setCheckable(True)
+        chnl1Button.setText("on/off")
+
+        chnl2Button = QPushButton()
+        chnl2Button.setCheckable(True)
+        chnl2Button.setText("on/off")
+
+        chnl3Button = QPushButton()
+        chnl3Button.setCheckable(True)
+        chnl3Button.setText("on/off")
+
+        chnl4Button = QPushButton()
+        chnl4Button.setCheckable(True)
+        chnl4Button.setText("on/off")
+
+        # place widgets
+        layout = QGridLayout()
+        layout.addWidget(chnl1Label, 0, 0)
+        layout.addWidget(chnl2Label, 0, 1)
+        layout.addWidget(chnl3Label, 0, 2)
+        layout.addWidget(chnl4Label, 0, 3)
+
+        layout.addWidget(chnl1Button, 1, 0)
+        layout.addWidget(chnl2Button, 1, 1)
+        layout.addWidget(chnl3Button, 1, 2)
+        layout.addWidget(chnl4Button, 1, 3)
+        self.setLayout(layout)
 
         # show the window
         self.show()
